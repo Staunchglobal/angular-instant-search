0e2a92d1504a239d09e3628578a421b857e65d77
Saqib Shahzad <mac@macs-MacBook-Pro.local>
2019-12-11 14:22:03 +0500

    Add functionality of carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import * as algoliasearch from 'algoliasearch/lite';
 import { environment } from 'src/environments/environment';
 
+import { OwlOptions } from 'ngx-owl-carousel-o';
+
 const searchClient = algoliasearch(
   environment.appID,
   environment.appKey
@@ -13,6 +15,31 @@ const searchClient = algoliasearch(
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      }
+    },
+    nav: true
+  };
+
   config = {
     indexName: environment.indexName,
     searchClient
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { NgAisModule } from 'angular-instantsearch';
 import { AgmCoreModule } from '@agm/core';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { GeoSearchComponent } from './geo-search-markers/geo-search-markers.component';
@@ -13,8 +16,10 @@ import { environment } from 'src/environments/environment';
     GeoSearchComponent
   ],
   imports: [
+    CarouselModule,
     NgAisModule.forRoot(),
     BrowserModule,
+    BrowserAnimationsModule,
     AgmCoreModule.forRoot({
       apiKey: environment.googleMapKey
     })
